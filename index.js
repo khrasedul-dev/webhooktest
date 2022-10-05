0f7e559b5426e7b42f6a8a8dbbab7bb67ce2492c
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-10-06 03:57:54 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const {Telegraf} = require('telegraf')
+
+const bot = new Telegraf(process.env.TOKEN)
+
+
+bot.start(ctx=>{
+    ctx.reply('Bot started')
+})
+
+bot.command('test',ctx=>{
+    ctx.reply('This is test command')
+})
+
+bot.command('test2',ctx=>{
+  ctx.reply('hi')
+})
+
+bot.telegram.setWebhook(`${process.env.DOMAIN}:8443/${process.env.TOKEN}`)
+
+bot.startWebhook(`/${process.env.TOKEN}`,null,8443);
+
+bot.launch().then(()=>console.log("the bot is running on glitch")).catch((e)=>console.log(e))
